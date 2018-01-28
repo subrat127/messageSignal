7b3bc9c563675da5fc3248e9032c32ea65fc0ed8
Subrat Mishra <subrat127@gmail.com>
2018-01-28 23:41:58 +0530

    sixth commit

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080 ;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
+ console.log('server.js started');
 var http = require('http');
 
 
 // Loading the index file . html displayed to the client
 var server = http.createServer(function(req, res) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end("Hi Welcome to first Project.! ");
+    console.log('http request received');
+    res.writeHead(200, {"Content-Type": "text/html"});    
+    res.write('Hello World!'); 
+    res.end();
     
 });
 
