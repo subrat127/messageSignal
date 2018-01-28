ef7ea38319478905f2f75669d41757d9d6ebbf04
Subrat Mishra <subrat127@gmail.com>
2018-01-29 02:09:03 +0530

    eighth commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080 ;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
- console.log('server.js started');
+ console.log('server.js started!!!');
 var http = require('http');
 
 
@@ -15,12 +15,17 @@ var server = http.createServer(function(req, res) {
 });
 
 // Loading socket.io
-var io = require('socket.io',{ transports: ['websocket']}).listen(server);
+var io = require('socket.io').listen(server);
+io.set('transports', ['websocket']);
 
 // When a client connects, we note it in the console
 io.sockets.on('connection', function (socket) {
     console.log('A client is connected!');
 });
 
+io.on('connection', function(socket) {
+  console.log('Client connected');
+      
+socket.on('disconnect', fuction() { console.log('Client disconnected');});}
 
 server.listen(8080);
