da50db10a4e5190d5d6c5ebb05baf973febd2ac8
subrat127 <17098947+subrat127@users.noreply.github.com>
2018-01-29 09:50:28 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080 ;var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
- console.log('server.js started!!!');var http = require('http');
+ console.log('server.js started!!!123');var http = require('http');
 
-var server = http.createServer(function(req, res) {    console.log('http request received');    res.writeHead(200, {"Content-Type": "text/html"});        res.write('Hello World!');     res.end();    });
+var server = http.createServer(function(req, res) {    console.log('http request received!!!');    res.writeHead(200, {"Content-Type": "text/html"});        res.write('Hello World!');     res.end();    });
 var io = require('socket.io').listen(server);io.set('transports', ['websocket']);
 io.sockets.on('connection', function (socket) {    console.log('A client is connected!');});
 io.on('connection', function(socket) {  console.log('Client connected');      socket.on('disconnect', function(event) {     console.log('Client disconnected');    });        });
